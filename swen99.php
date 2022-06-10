d409bb8d171648bc63742cdf5e0d2cc2e0bc2d9e
Swen <a159856a@gmail.com>
2022-06-10 15:51:46 +0800

    0351

---
+++ swen99.php
@@ -0,0 +1,26 @@
+<h1>九九乘法表</h1>
+<hr />
+
+<table border="1" width="100%">
+
+
+    <?php
+
+    for ($k = 0; $k < 2; $k++) {
+        echo "<tr>";
+        for ($j = 2; $j <= 5; $j++) {
+            $newj = $j + $k * 4;
+            echo "<td>";
+            for ($i = 1; $i <= 9; $i++) {
+                $r = $newj  * $i;
+                echo "{$newj} x {$i} = {$r}<br />";
+            }
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+
+
+    ?>
+
+</table>
