b24f24328c0fbb73f03c103e28709624bc22bacc
Swen <a159856a@gmail.com>
2022-06-10 13:58:05 +0800

    0610

--- swen06.php
+++ swen06.php
@@ -1,17 +1,35 @@
 <?php
-    $x = $y = $r = ''; 
+$x = $y = $r = $op = '';
 if (isset($_GET['x']) && isset($_GET['y'])) {
     $x = $_GET['x'];
     $y = $_GET['y'];
-    $r = $x + $y;
+    $op = $_GET['op'];
+    switch ($op) {
+        case '1':
+            $r = $x + $y;
+            break;
+        case '2':
+            $r = $x - $y;
+            break;
+        case '3':
+            $r = $x * $y;
+            break;
+        case '4':
+            $r = $x / $y;
+            break;
+    }
 }
-
 ?>
 
 <form action="swen06.php">
-    <input name="x" value="<?php echo $x ?>"/>
-    +
-    <input name="y" value="<?php echo $y ?>"/>
+    <input name="x" value="<?php echo $x ?>" />
+    <select name="op">
+        <option value="1" <?php echo ($op == '1' ? 'selected' : ''); ?>>+</option>
+        <option value="2" <?php echo ($op == '2' ? 'selected' : ''); ?>>-</option>
+        <option value="3" <?php echo ($op == '3' ? 'selected' : ''); ?>>*</option>
+        <option value="4" <?php echo ($op == '4' ? 'selected' : ''); ?>>/</option>
+    </select>
+    <input name="y" value="<?php echo $y ?>" />
     <input type="submit" value="=" />
     <span><?php echo $r ?></span>
 </form>
