954db76a44f2610295e27d4c8263e5a81de6f094
Swen <a159856a@gmail.com>
2022-06-10 16:28:28 +0800

    填色

--- swen99.php
+++ swen99.php
@@ -6,16 +6,21 @@
 
     <?php
 
-    define('ROWS', 2);
+    define('ROWS', 3);
     define('START', 2);
-    define('COLS', 4);
+    define('COLS', 3);
 
 
     for ($k = 0; $k < ROWS; $k++) {
         echo "<tr>";
         for ($j = START; $j < START + COLS; $j++) {
-            $newj = $j + $k * COLS;
-            echo "<td>";
+            $newj = $j + $k * COLS; 
+            if(($j+$k)%2==0){
+                echo "<td bgcolor='pink'>";
+            }else{
+                echo "<td bgcolor='yellow'>";
+            }
+            
             for ($i = 1; $i <= 9; $i++) {
                 $r = $newj  * $i;
                 echo "{$newj} x {$i} = {$r}<br />";
